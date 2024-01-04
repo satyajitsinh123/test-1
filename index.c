d13a57cbe360f4c6e50dbdc5d6b204c4920329d8
satyajitsinh123 <satyajitsinh63@gamil.com>
2024-01-04 12:01:17 +0530

    hello

---
+++ index.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main(){
+    // que-1
+
+
+
+for(int i=1; i<=9;i++){
+    printf("* ");
+}
+printf("\n");
+for(int i=1; i<=9;i++){
+    printf("* ");
+}
+printf("\n");
+for(int i=1; i<=9;i++){
+    printf("* ");
+}
+printf("\n");
+for(int i=1; i<=9;i++){
+    printf("* ");
+}
+printf("\n");
+
+
+
+// que-2
+int num=26;
+int odd;
+int even;
+for(i=1;i<=num;i++)
+if(num);
+{
+    printf("odd");
+}
+else{
+    
+        printf("even");
+    
+}
+
+
+
+
+return 0;
+}
